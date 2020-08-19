a63daa47aeed09ce21180cbbd4f3157b4d880c17
Oliver Strik <oliverstrik@gmail.com>
2020-08-19 10:08:45 +0200

    Converted skew to a octoprint plugin.
    Haven't tested it yet. Could be full of mistakes.

--- skew.py
+++ skew.py
@@ -1,126 +1,38 @@
 #!/usr/bin/env python3
 import re
-import os
-import argparse
+import octoprint.plugin
+import octoprint.filemanager
+import octoprint.filemanager.util
 
-parser = argparse.ArgumentParser(
-    description='gskewer is a tool to skew the coordinates in a G-code file.'
-)
+class gskewer(octoprint.filemanager.util.LineProcessorStream):
+    def __init__(self, input_stream, xytan, yztan, zxtan):
+        super(input_stream)
+        
+        self.xytan = xytan
+        self.yztan = yztan
+        self.zxtan = zxtan
+        
+        if not zxtan == 0:
+        print('The ZX error is set to', zxtan, 'degrees')
 
-xygroup = parser.add_mutually_exclusive_group(required=True)
-xygroup.add_argument(
-    '--xyerr',
-    type=float,
-    help='Error in the X-axis for the XY pair in mm.'
-)
-xygroup.add_argument(
-    '--xytan',
-    type=float,
-    help='The error in the XY pair as a tangent. (xyerr/xylen)'
-)
+        if xytan == 0.0 and yztan == 0.0 and zxtan == 0.0:
+            print('No skew parameters provided. Nothing will be done.')
 
+        filename = args.file
 
-yzgroup = parser.add_mutually_exclusive_group(required=True)
-yzgroup.add_argument(
-    '--yzerr',
-    type=float,
-    help='Error in the Y-axis for the YZ pair in mm.'
-)
-yzgroup.add_argument(
-    '--yztan',
-    type=float,
-    help='The error in the YZ pair as a tangent. (yzerr/yzlen)'
-)
+        outname = re.sub(r'.gcode', '-skewed.gcode', filename)
 
+        xin = 0.0
+        yin = 0.0
+        zin = 0.0
+        
+        #if os.path.isfile(outname):
+        #os.remove(outname)
 
-zxgroup = parser.add_mutually_exclusive_group(required=True)
-zxgroup.add_argument(
-    '--zxerr',
-    type=float,
-    help='Error in the Z-axis for the ZX pair in mm.'
-)
-zxgroup.add_argument(
-    '--zxtan',
-    type=float,
-    help='The error in the ZX pair as a tangent. (zxerr/zxlen)'
-)
-
-
-parser.add_argument(
-    '--xylen',
-    default=100,
-    type=int,
-    help='Length of the side where the XY error measurement was taken. This is optional, default value is 100mm'
-)
-parser.add_argument(
-    '--yzlen',
-    default=100,
-    type=int,
-    help='Length of the side where the YZ error measurement was taken. This is optional, default value is 100mm'
-)
-parser.add_argument(
-    '--zxlen',
-    default=100,
-    type=int,
-    help='Length of the side where the ZX error measurement was taken. This is optional, default value is 100mm'
-)
-
-# input file setup. name of input file is not in "args.file"
-parser.add_argument("file", type=str)
-
-# parser.add_argument('-v', action='count', help='Increases terminal output verbosity. More V's will increase verbosity' )
-
-args = parser.parse_args()
-
-if args.xytan:
-    xytan = args.xytan
-elif args.xyerr:
-    xytan = args.xyerr / args.xylen
-else:
-    xytan = 0.0
-
-if not xytan == 0:
-    print('The XY error is set to', xytan, 'degrees')
-
-if args.yztan:
-    yztan = args.yztan
-elif args.yzerr:
-    yztan = args.yzerr / args.yzlen
-else:
-    yztan = 0.0
-
-if not yztan == 0:
-    print('The YZ error is set to', yztan, 'degrees')
-
-if args.zxtan:
-    zxtan = args.zxtan
-elif args.zxerr:
-    zxtan = args.zxerr / args.zxlen
-else:
-    zxtan = 0.0
-
-if not zxtan == 0:
-    print('The ZX error is set to', zxtan, 'degrees')
-
-if xytan == 0.0 and yztan == 0.0 and zxtan == 0.0:
-    print('No skew parameters provided. Nothing will be done.')
-
-filename = args.file
-
-outname = re.sub(r'.gcode', '-skewed.gcode', filename)
-
-xin = 0.0
-yin = 0.0
-zin = 0.0
-
-if os.path.isfile(outname):
-    os.remove(outname)
-
-outfile = open(outname, 'a')
-
-with open(filename, 'r') as infile:
-    for line in infile:
-        # Check that the current 'line' is a move, if so the line is processed
+        #outfile = open(outname, 'a')
+        
+        
+    def process_line(self, line):
         gmatch = re.match(r'G[0-1]', line, re.I)
         if gmatch:
             print('line was a G0/G1 command!')
@@ -163,7 +75,26 @@ with open(filename, 'r') as infile:
                 lineout = re.sub(r'[zZ]\d*\.*\d*', 'Z' + str(zout), lineout)
 
             print('new line: ', lineout)
-            outfile.write(lineout)
+            return lineout
         else:
             print('Skipping, not a movement.', line)
-            outfile.write(line)
+            return line
+
+def skew_gcode(path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
+    if not octoprint.filemanager.valid_file_type(path, type="gcode"):
+        return file_object
+    
+    #import os
+    #name, _ = os.path.splitext(file_object.filename)
+    #if not name.endswith("_skew"):
+    #    return file_object
+    
+    return octoprint.filemanager.util.StreamWrapper(file.object.filename, GSkewer(file_object.stream(), 0, 0, 0))
+
+__plugin_name__ = "gskewer"
+__plugin_description__ = "Skews G0 and G1 commands to correct for error in each axis. Does not skew G2 or G3, so use on such gcode at your own risk."
+__plugin_pythoncompat__ = ">=3.7, <4"
+__plugin_hooks__ = {
+    "octoprint.filemanager.preprocessor": skew_gcode
+}
+
